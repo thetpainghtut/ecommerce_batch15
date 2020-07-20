ef414a1ca6c9b7c8538c7129e94de17d2d2190c4
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-07-20 18:39:45 +0800

    edited from server for auth testing

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -12,7 +12,7 @@ class ItemController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api')->except('index','show');
+        $this->middleware('auth:api');
     }
     /**
      * Display a listing of the resource.
